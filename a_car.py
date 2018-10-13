9a7ad600fcfb1da02518fd296177efa8cdb57bb9
Marcelo Gobelli <marcelo.gobelli@gmail.com>
2018-10-13 10:05:50 -0700

    back with self

--- a_car.py
+++ a_car.py
@@ -1,11 +1,11 @@
 class Car():
     def __init__(self, make, model, year):
-        make = make
+        self.make = make
         self.model = model
         self.year = year
         self.odometer_reading = 0
     def get_descriptive_name(self):
-        long_name = str(self.year) + ' ' + make  + ' ' + self.model
+        long_name = str(self.year) + ' ' + self.make  + ' ' + self.model
         return long_name.title()
     def read_odometer(self):
         print("this car has " + str(self.odometer_reading) + " miles in it")
