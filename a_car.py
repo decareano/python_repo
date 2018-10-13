18b6c0c9766340588aeb85558e5d3a0fdc61a92e
Marcelo Gobelli <marcelo.gobelli@gmail.com>
2018-10-13 10:18:01 -0700

    adding a method

--- a_car.py
+++ a_car.py
@@ -7,9 +7,12 @@ class Car():
     def get_descriptive_name(self):
         long_name = str(self.year) + ' ' + self.make  + ' ' + self.model
         return long_name.title()
-    def read_odometer(self):
+    def read_odometer(self, mileage):
         print("this car has " + str(self.odometer_reading) + " miles in it")
+    def update_odometer(self, mileage):
+        self.odometer_reading = mileage
 
 my_new_car = Car('audi', 'a4', 2016)
 print(my_new_car.get_descriptive_name())
 my_new_car.read_odometer(23)
+my_new_car.odometer_reading()
