5a6d82deeaa104f5d66f6b55050437bfb640b141
Marcelo Gobelli <marcelo.gobelli@gmail.com>
2018-10-20 13:36:57 -0700

    inheritance play

--- a_car.py
+++ a_car.py
@@ -31,3 +31,10 @@ my_new_car.read_odometer()
 
 my_new_car.increment_odometer(100)
 my_new_car.read_odometer()
+
+class ElectricCar(Car):
+    def __init__(self, make, model, year):
+        super().__init__(make, model, year)
+
+my_tesla = ElectricCar('tesla', 'model s', 2016)
+print(my_tesla.get_descriptive_name())
