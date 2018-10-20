c27c11848a4d43eecda677aa7014d21f2a0411f8
Marcelo Gobelli <marcelo.gobelli@gmail.com>
2018-10-20 13:47:58 -0700

    battery added to electric car

--- a_car.py
+++ a_car.py
@@ -35,6 +35,11 @@ my_new_car.read_odometer()
 class ElectricCar(Car):
     def __init__(self, make, model, year):
         super().__init__(make, model, year)
+        self.battery_size = 70
+
+    def describe_battery(self):
+        print("this car has a " + str(self.battery_size) + " -kWh battery")
 
 my_tesla = ElectricCar('tesla', 'model s', 2016)
 print(my_tesla.get_descriptive_name())
+my_tesla.describe_battery()
