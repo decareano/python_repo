4a8771a2cc300fe3f49d4756600c1ef222cc5f3c
Marcelo Gobelli <mgobelli@Mac-loaner02.local>
2018-11-22 22:36:13 -0800

    positional params

--- describe_pet.py
+++ describe_pet.py
@@ -2,3 +2,15 @@ def describe_pet(animal_type, pet_name):
   print("\nI have a " + animal_type + ".")
   print("My " + animal_type + "'s name is " + pet_name.title() + ".")
 describe_pet('hamster', 'harry')
+
+def make_shirt(size):
+	print("my shirt is " + size + " and it has written: I love python")
+
+make_shirt('large')
+
+def make_shirt(message, size="large"):
+        print("\n" " my shirt is " + size + " and the label is " + message)
+make_shirt("I love c")
+make_shirt("I love python", size="medium")
+make_shirt("I dislike perl", size="any")
+
