5625baa68076a55e378093956042a7f7963a96aa
Marcelo Gobelli <mgobelli@Mac-loaner02.local>
2018-11-24 20:19:02 -0800

    returning a dict

--- describe_pet1.py
+++ describe_pet1.py
@@ -16,10 +16,28 @@ formatted_name = get_formatted_name(f_name, l_name)
 print("\nHello, " + formatted_name + "!")
 
 def make_album(artist_name, album_title, tracks=11):
-  artist = { 'name': artist_name, 'album': album_title, 'number': tracks }
+  # artist = { 'name': artist_name, 'album': album_title, 'number': tracks }
+  artist = artist_name + ' ' +  album_title
+  
   return artist
 
-my_album = make_album('madonna', 'world tour')
-my_album1 = make_album('billy joel', 'tour of force', 14)
-my_album2 = make_album('hendrix', 'purple haze')
-print(my_album, my_album1, my_album2)
+my_album1 = make_album('madonna', 'world tour')
+my_album2 = make_album('billy joel', 'tour of force', 14)
+my_album3 = make_album('hendrix', 'purple haze')
+print(my_album1, my_album2, my_album3)
+
+while True:
+  print("\nPlease pick an album: ")
+  print("(enter 'q' to quit)")
+ 
+  i_artist = input("artist: ")
+  if i_artist == 'q':
+    break
+
+  i_album = input("album: ")
+  if i_album == 'q':
+    break
+
+format_name = make_album(i_artist, i_album)
+print("\nHello, " + format_name +  "!")
+
