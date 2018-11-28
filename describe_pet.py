fa0a518dc7edee17cca237b1fe79406fd2072994
Marcelo Gobelli <marcelo.gobelli@gmail.com>
2018-11-28 07:49:36 -0800

    dunder_work

--- describe_pet.py
+++ describe_pet.py
@@ -2,3 +2,15 @@ def describe_pet(animal_type, pet_name):
   print("\nI have a " + animal_type + ".")
   print("My " + animal_type + "'s name is " + pet_name.title() + ".")
 describe_pet('hamster', 'harry')
+
+def describe_pet(animal_type1, pet_name1):
+  print("\nI have a " + animal_type1 + ".")
+  print("My " + animal_type1 + "'s name is " + pet_name1.title() + ".")
+describe_pet(animal_type1='hamster', pet_name1='harry')
+
+def make_shirt(size='large'):
+    print("\nI have a that says: I love python")
+
+
+make_shirt(size)
+
