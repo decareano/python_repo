40c8903f15fee82d700131cea9d695e7e9877268
Marcelo Gobelli <marcelo.gobelli@gmail.com>
2018-12-15 23:20:12 -0800

    adding pizza loop

---
+++ more_pizza.py
@@ -0,0 +1,17 @@
+favorite_pizzas = ['pepperoni', 'hawaiian', 'veggie']
+
+# Print the names of all the pizzas.
+for pizza in favorite_pizzas:
+    print(pizza)
+
+print("\n")
+
+# Print a sentence about each pizza.
+for pizza in favorite_pizzas:
+    print("I really love " + pizza + " pizza!")
+
+print("\nI really love pizza!")
+
+numbers = list(range(1, 21))
+for i in numbers:
+    print(i)
