54fab45ddc66fa0d5a6fa887c61a8a106cfa9507
Marcelo Gobelli <mgobelli@Mac-loaner02.local>
2019-01-07 10:54:10 -0800

    coding predict

--- passing_list.py
+++ passing_list.py
@@ -11,7 +11,7 @@ completed_models = []
 
 while unprinted_designs:
   current_design = unprinted_designs.pop()
-  print("Printing model: " + current_design
+  print("Printing model: " + current_design)
   completed_models.append(current_design)
 
 print("\nThe following models have been printed:")
