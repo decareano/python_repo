38e836da28fd9d9141225907ecb0d3f35df2f6b6
Marcelo Gobelli <marcelo@marcelo.gateway.sonic.net>
2019-03-18 17:10:00 -0700

    birthday candlesHR

---
+++ birthday_cake_HR.py
@@ -0,0 +1,11 @@
+candles_cake = int(input())
+candles_height = list(map(int, input().split()))
+count = 0
+
+#for _ in range(candles_cake):
+    #for i in range(len(candles_height)):
+        #if (i==dups):
+            #count=count+1
+max(candles_height)
+        
+print(candles_height.count(max(candles_height)))
