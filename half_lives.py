84b28526390c1894e0e0880b0cc27a3b2e302680
Marcelo Gobelli <marcelo@marcelo.gateway.sonic.net>
2019-03-25 15:50:45 -0700

    more samples

---
+++ half_lives.py
@@ -0,0 +1,16 @@
+name = input("who are you")
+print("hello %s" % (name,))
+
+
+def first(name):
+    name = "'g'"
+    print("%s" % (name))
+
+first("")
+
+
+string = ""
+s = "abcd:"
+
+str1, str2 = s.split(':')
+str1
