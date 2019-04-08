cf9931ac0f1900bcb03411191000e4e54b47b96f
Marcelo Gobelli <marcelo@marcelo.gateway.sonic.net>
2019-04-07 23:00:25 -0700

    4moreExercises

---
+++ map_list_compre.py
@@ -0,0 +1,13 @@
+numbers = [4, 2, 1, 6, 9, 7]
+def square(x):
+  return x*x + 1
+
+def is_odd(x):
+    return bool(x%2)
+
+
+# map(funtion, iterable, ...) it applies function to every member of iter and returns a result
+
+def get_name_and_decades(name, age):
+    print("my name is {name} and I am {age /10:.5f} decades old")
+
--- test_selenium.py
+++ test_selenium.py
@@ -1,7 +1,10 @@
 import urllib3
 from bs4 import BeautifulSoup
-http = urllib3.PoolManager()
-r = http.request('GET', 'https://http://www.afa.com.ar/deposito/html/v3/index.html?channel=deportes.futbol.primeraa.448792&lang=es_LA')
-soup = BeautifulSoup(r.data, 'div')
+from selenium import webdriver
+browser = webdriver.Firefox()
+browser.get('http://www.racefans.net/2019/03/31/2019-bahrain-grand-prix-interactive-data-lap-charts-times-and-tyres/')
+# http = urllib3.PoolManager()
+# r = http.request('GET', 'http://www.racefans.net/2019/03/31/2019-bahrain-grand-prix-interactive-data-lap-charts-times-and-tyres/')
+soup = BeautifulSoup(r.data, 'html.parser')
 print (soup.title)
 print (soup.title.text)
