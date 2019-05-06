1cf63de3ad5bdc4ea483e200798c2832352b6fa2
Marcelo Gobelli <marcelo@marcelo.gateway.sonic.net>
2019-05-05 21:23:48 -0700

    restaurant test

---
+++ sum_array_HR.py
@@ -0,0 +1,12 @@
+def _sum(arr, n):
+    return(sum(arr))
+
+#driver function
+arr=[]
+#input values to list
+arr = [12, 3, 4, 15]
+n = len(arr)
+
+ans = _sum(arr, n)
+
+print('Sum of array is ', ans)
