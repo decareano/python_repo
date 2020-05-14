ab8d3f5dcf7e8c57c6fd85741834ee2e0f43aef8
Marcelo Gobelli <marcelo@marcelos-mbp.gateway.sonic.net>
2020-05-13 17:52:24 -0700

    adding portfolio.py

---
+++ investor_user.py
@@ -0,0 +1,18 @@
+class User:
+    def __init__(self, name, budget=10000):
+        self.name = name
+        #self.portfolio_account = portfolio_account
+        self.budget = budget
+        self.stocks = []
+        #self.bank_account = bank_account
+
+    
+
+
+# p1 = Portfolio()  #shorthand for: object.__call__()
+# u1 = User("Marcelo", portfolio_account=p1, budget=10000)
+# print(callable(p1))
+# #s1()
+# print(u1.name)
+# u1.buy_stock()
+
