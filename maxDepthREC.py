0ec5624901dd4a7c6c95a741d97c83d1021992ee
Marcelo Gobelli <marcelogobelli@Marcelos-MacBook-Pro-3.local>
2024-03-19 09:45:48 -0700

    comments on recursive function

--- maxDepthREC.py
+++ maxDepthREC.py
@@ -8,28 +8,25 @@ class node:
         self.right=right
         self.node=node
   
-    # Function to create a new
-    # Binary node
-
 #function for traversal is t()
 class Solution:       
     def maxDepth(self, root):
         if not root:
             return 0
-        #print(self.maxDepth(root.left))
-        #print(self.maxDepth(root.right))
-        #print(root.left)
-        #print(root.right)
+        #main point in recursive function is to find a None value
+        #and start backing up from there.
+        #easier said than done 
+        #but I am beginning to get a handle of how it's don
         return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
         
-        
-       
-root = node(1)
-root.left = node(2)
-root.right = node(3)
+root = node(3)
+root.left = node(9)
+root.right = node(20)
 root.left.left = node(4)
-root.left.right = node(5)
-root.right.left = node(6)
+root.left.right = node(12)
+root.left.right.left= node(3)
+root.right.left = node(15)
 root.right.right = node(7)
+#root.left.right.left(3)
 myVar = Solution()
 myVar.maxDepth(root)
