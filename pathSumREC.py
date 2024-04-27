ea1ca8b46fca76b3bff1d455bdff8c4b653fc8f5
Marcelo Gobelli <marcelogobelli@Marcelos-MacBook-Pro-3.local>
2024-04-26 23:15:02 -0700

    pathSumRECURSIVE with help from discuss

---
+++ pathSumREC.py
@@ -0,0 +1,42 @@
+# Definition for a binary tree node.
+class node:
+     def __init__(self, val=0, left=None, right=None):
+         self.val = val
+         self.left = left
+         self.right = right
+
+class Solution:
+    def pathSum(self, root: node, targetSum: int) -> int:
+        self.count = 0
+        self.prefix_sum = {0: 1}
+        self.dfs(root, targetSum, 0)
+        return self.count
+        
+    def dfs(self, node: node, targetSum: int, curr_sum: int) -> None:
+        if not node:
+            return
+        
+        curr_sum += node.val
+        self.count += self.prefix_sum.get(curr_sum - targetSum, 0)
+        
+        self.prefix_sum[curr_sum] = self.prefix_sum.get(curr_sum, 0) + 1
+        
+        self.dfs(node.left, targetSum, curr_sum)
+        self.dfs(node.right, targetSum, curr_sum)
+        
+        self.prefix_sum[curr_sum] -= 1
+        
+        
+  
+root = node(10)
+root.left = node(5)
+root.right = node(-3)
+root.right.right = node(11)
+root.left.left = node(3)
+root.left.right = node(2)
+root.left.left.left = node(3)
+root.left.left.right = node(-2)
+root.left.right.left = None
+root.left.right.right = node(1)
+myVar = Solution()
+myVar.pathSum(root, 8)
