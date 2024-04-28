d5e676e1fcb990b8adbf261d952d3fdbe0b900ac
Marcelo Gobelli <marcelogobelli@Marcelos-MacBook-Pro-3.local>
2024-04-27 17:48:42 -0700

    adding comments

--- pathSumREC.py
+++ pathSumREC.py
@@ -1,4 +1,5 @@
 # Definition for a binary tree node.
+# seminal exercise to understand recursion
 class node:
      def __init__(self, val=0, left=None, right=None):
          self.val = val
@@ -6,26 +7,33 @@ class node:
          self.right = right
 
 class Solution:
-    def pathSum(self, root: node, targetSum: int) -> int:
+    # would be nice to do it with 
+    # a pathSum and a helper inside pathSum(no self)
+    def pathSum(self, root, targetSum):
         self.count = 0
         self.prefix_sum = {0: 1}
         self.dfs(root, targetSum, 0)
         return self.count
         
-    def dfs(self, node: node, targetSum: int, curr_sum: int) -> None:
+    def dfs(self, node, targetSum, curr_sum):
         if not node:
             return
         
         curr_sum += node.val
+        
+        #below adds to the count if curr - target not in dict
         self.count += self.prefix_sum.get(curr_sum - targetSum, 0)
-        
+        print(self.prefix_sum.get(curr_sum - targetSum, 0))
+        #below adds to the dict: value and 1 meaning:
+        #we found a key. after processing it, it goes to zero
         self.prefix_sum[curr_sum] = self.prefix_sum.get(curr_sum, 0) + 1
-        
+        #below runs the recursive calls
         self.dfs(node.left, targetSum, curr_sum)
         self.dfs(node.right, targetSum, curr_sum)
         
         self.prefix_sum[curr_sum] -= 1
-        
+        #sequence: once path is visited, it goes to 0
+        #difficult to follow
         
   
 root = node(10)
