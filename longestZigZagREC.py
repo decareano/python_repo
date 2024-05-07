bbec9ef4659492fa17bc2d306a23607dcb26eec6
Marcelo Gobelli <marcelogobelli@Marcelos-MacBook-Pro-3.local>
2024-05-06 20:02:20 -0700

    longestZigZagREC took me a while...and still has questions.

---
+++ longestZigZagREC.py
@@ -0,0 +1,45 @@
+# Definition for a binary tree node.
+# seminal exercise to understand recursion
+class node:
+   
+     def __init__(self, val=0, left=None, right=None):
+         self.val = val
+         self.left = left
+         self.right = right
+
+class Solution:
+    
+    def longestZigZag(self, root):
+        self.goDir = 0
+       
+        def dfs(node, zigDir, pathLen):
+            
+            if node:
+                self.goDir = max(self.goDir, pathLen)
+                print(self.goDir)
+                if zigDir:
+                    #pathLen = max(maxPathLen, pathLen)
+                    dfs(node.left, False, pathLen + 1)
+                    dfs(node.right, True, 1)
+               
+                else:
+                    dfs(node.left, False, 1)
+                    dfs(node.right, True, pathLen + 1)
+                
+        dfs(root, False, 0) 
+        dfs(root, True, 0)
+    
+        return self.goDir
+  
+root = node(1)
+root.right = node(1)
+root.right.left = node(1)
+root.right.right = node(1)
+root.right.right.left = node(1)
+root.right.right.right = node(1)
+root.right.right.left.right = node(1)
+root.right.right.left.right.right = node(1)
+
+
+myVar = Solution()
+myVar.longestZigZag(root)
