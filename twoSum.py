845a548274d7cfcd2fc3ded2d5fa5186931b58d2
Marcelo Gobelli <almagro@comcast.net>
2025-06-08 16:18:29 -0700

    Update twoSum.py

--- twoSum.py
+++ twoSum.py
@@ -8,3 +8,16 @@ class Solution:
                 if complement in result:
                     return [result[complement], i]
                 result[nums[i]] = i
+
+
+
+JUNE 2025 Use this solution:
+
+class Solution:
+    def twoSum(self, nums, target):
+        pair_idx = {}
+        for i, num in enumerate(nums):
+            print(i, num)
+            if target - num in pair_idx:
+                return [i, pair_idx[target - num]]
+            pair_idx[num] = i
